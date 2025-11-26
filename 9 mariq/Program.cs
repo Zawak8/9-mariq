118d9fc1731eaef4ec849923dd8991546fe57290
DINDEV <viktordindev@gmail.com>
2025-11-26 13:06:23 +0200

    Add project files.

---
+++ 9 mariq/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _9_mariq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  5x20
+
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 || i == n - 1)
+                    {
+                        if (j == 0 || j == n - 1)
+                        {
+                            Console.Write("+ ");
+                        }
+                        else
+                        {
+                            Console.Write("- ");
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
